44840f2970f53cbea2f6d4cc9b091cdf67ce235e
TeeNak <teenak77@gmail.com>
2018-03-06 17:10:43 +0900

    seed in initializer

--- Odata4AspNet/Migrations/Configuration.cs
+++ Odata4AspNet/Migrations/Configuration.cs
@@ -18,32 +18,5 @@ namespace Odata4AspNet.Migrations
             ContextKey = "Odata4AspNet.Models.ProductsContext";
         }
 
-        protected override void Seed(Odata4AspNet.Models.ProductsContext context)
-        {
-            // Requred to issue Update-Database to fill seed data.
-
-            /*
-            context.Products.AddOrUpdate(x => x.Id,
-                new Product() { Id = 1, Name = "Canned Juice", Price = 60.00M, Category = "Food" },
-                new Product() { Id = 2, Name = "Fried chicken", Price = 120.00M, Category = "Food" },
-                new Product() { Id = 3, Name = "Hamburger", Price = 150.00M, Category = "Food" }
-            );
-            */
-
-
-
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            string resourceName = "Odata4AspNet.Models.SeedData.products.csv";
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            {
-                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
-                {
-                    CsvReader csvReader = new CsvReader(reader);
-                    var products = csvReader.GetRecords<Product>().ToArray();
-                    context.Products.AddOrUpdate(c => c.Id, products);
-                }
-            }
-            
-        }
     }
 }
